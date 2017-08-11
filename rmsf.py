84315d671d99a730f7b908d432470c2c13fd3af5
Andrew Natale <anatale@otp589354ots.ucsf.edu>
2017-08-11 13:25:42 -0700

    1

--- rmsf.py
+++ rmsf.py
@@ -10,11 +10,16 @@ tag = sys.argv[4]
 basename = ''.join(traj.split('/')[-1].split('.')[:-1])
 print(basename)
 
+if basename.startswith('traakTM4'):
+    selecttext = 'protein and ((resid 102-106) or (resid 211 to 215) or (resid 384 to 388) or (resid 493 to 497)) and name CA'
+else:
+    selecttext = 'protein and (resid 129-133 or resid 238-242) and name CA'
+
 a = RMSFwindow()
 a.load_traj(topo,traj,50)
 # 41 frames == ~2ns time window
 # using an odd number means the windows are centered on existing time steps
-a.run('protein and (resid 129-133 or resid 238-242) and name CA', 41)
+a.run(selecttext, 41)
 
 os.chdir(output_dir)
 a.write_data('%s.%s' % (basename, tag))
